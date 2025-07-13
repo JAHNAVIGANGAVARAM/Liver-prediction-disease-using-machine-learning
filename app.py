d201beacd12719811d97ee18dbcb752206130ae0
JAHNAVIGANGAVARAM <Jahnavi.22bce20426@vitapstudent.ac.in>
2025-07-14 01:49:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,5 +46,9 @@ def predict():
         print(f"Error during prediction: {e}")
         return jsonify({'prediction_result': 'An error occurred during prediction.'})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
